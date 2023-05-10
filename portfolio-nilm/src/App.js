e9a18bbd29deb8f9032bf3298aea1bad3248895c
Lindris <linda01harris@gmail.com>
2023-05-10 18:09:31 +0100

    code added from Nirmal

--- portfolio-nilm/src/App.js
+++ portfolio-nilm/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+import React from "react";
+import {Page1} from './Pages/Page1';
+import {Page2} from './Pages/Page2';
+import { Routes, Route } from "react-router-dom";
+function App(){
+  return(
+    <Routes><Route path='/' element={<Page1/>}/>
+    <Route path='about' element={<Page2/>}/>
+    </Routes>
+  )
 }
-
 export default App;
